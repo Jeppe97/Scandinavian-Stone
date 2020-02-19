6badb1871f2cd2269851164bb4701ee8a2f4ef5d
mrlingon <gustav.alvinsson@telia.com>
2020-02-19 11:53:22 +0100

    Removed MainContent component as it was useless.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/Header'
-import MainContent from './components/MainContent';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import WorkMethods from './components/WorkMethods';
 import Statistics from './components/Statistics';
