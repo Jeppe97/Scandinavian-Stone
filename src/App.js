defa1c41738c2da218dc7c491f57896375cdb1ac
mrlingon <gustav.alvinsson@telia.com>
2020-02-19 15:40:24 +0100

    fixed transitions

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/Header'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import WorkMethods from './components/WorkMethods';
 import Statistics from './components/Statistics';
-import {CSSTransition, TransitionGroup} from 'react-transition-group';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import MainMenu from './components/MainMenu';
 
 
 function App() {
   return (
     <div className="app-container">
-    <CSSTransition in={true} appear={true} timeout={800} classNames="fade">
-      <Header />
-      </CSSTransition>
-      
-      <Router>
-
       <CSSTransition in={true} appear={true} timeout={800} classNames="fade">
-      <Route render={({location}) => (
         <TransitionGroup>
-        <CSSTransition key={location.key} timeout={450} classNames="fade">
-        <Switch location={location}>
-        <Route path="/" exact component={MainMenu} /> 
-        <Route path="/workmethods" component={WorkMethods} />
-        <Route path="/statistics" component={Statistics} />
-        </Switch>
-        </CSSTransition>
+        <Header />
+          <Router>
+            <Route render={({ location }) => (
+              <TransitionGroup>
+                <CSSTransition key={location.key} timeout={450} classNames="fade">
+                  <Switch location={location}>
+                    <Route path="/" exact component={MainMenu} />
+                    <Route path="/workmethods" component={WorkMethods} />
+                    <Route path="/statistics" component={Statistics} />
+                  </Switch>
+                </CSSTransition>
+              </TransitionGroup>
+            )} />
+        </Router>
         </TransitionGroup>
-        )} />
         </CSSTransition>
-      </Router>
     </div>
   );
 }
