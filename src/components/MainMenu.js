8d5ce2a12946b75a165a808a0a135f8b0bb3ac1b
mrlingon <gustav.alvinsson@telia.com>
2020-02-19 16:20:49 +0100

    FIXED THE FIX

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import '../components/styles/MainMenu.scss';
 
 const MainMenu = () => {
     return(
         <div className="MainMenu">
-            <Link to="/workmethods"className="btn1">
-           <button onClick={ refreshPage } >RÄKNA</button>
+            <Link to="/workmethods">
+           <button>RÄKNA</button>
            </Link>
-           <Link to="/statistics"className="btn1">
-           <button onClick={ refreshPage } >STATISTIK</button>
+           <Link to="/statistics">
+           <button>STATISTIK</button>
            </Link>
-           <Link to="/"className="btn1">
-           <button onClick={ refreshPage } >LOGGA UT</button>
+           <Link to="/workmethods">
+           <button>LOGGA UT</button>
            </Link>
         </div>
     );
 }
-function refreshPage() {
-    window.location.assign("/");
-}
+
 export default MainMenu;
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,34 +1,44 @@
 import React, { Component } from "react";
-import {Link} from 'react-router-dom';
 import "../styles/Login.scss";
+
+
+window.onload=function(){
+    const adminLoginButton = document.getElementById('admin');
+    const userLoginButton = document.getElementById('user');
+    const container = document.getElementById('container');
+
+    adminLoginButton.addEventListener('click', () => {
+        container.classList.add("right-panel-active");
+    });
+    
+    userLoginButton.addEventListener('click', () => {
+        container.classList.remove("right-panel-active");
+    });
+}
 
 export class Login extends Component {    
     render(){
         return(
     <div className ="container" id="container">
+
              <div className="form-container admin-container">
-                        <form>
+                        <form action="#">
                             <h1>Admin</h1>
                             <input type="email" placeholder="Användarnamn" />
                             <input type="password" placeholder="Lösenord" />
                             <a href="#">Glömt ditt lösenord?</a>
-                            <Link to="/mainmenu" className="btn1">
                             <button>Logga In</button>
-                            </Link>
-
                         </form>
             </div>
 
             <div className="form-container sign-in-container">
-                        <form>
-                            <h1>Användare</h1>
-                            <input type="email" placeholder="Användarnamn" />
-                            <input type="password" placeholder="Lösenord" />
-                            <a href="#">Glömt ditt lösenord?</a>
-                            <Link to="/mainmenu" className="btn1">
-                            <button>Logga In</button>
-                            </Link>
-                        </form>
+                <form action="#">
+                    <h1>Användare</h1>
+                    <input type="email" placeholder="Användarnamn" />
+                    <input type="password" placeholder="Lösenord" />
+                    <a href="#">Glömt ditt lösenord?</a>
+                    <button>Logga In</button>
+                </form>
             </div>
             
             <div className="overlay-container">
@@ -50,19 +60,5 @@ export class Login extends Component {
         );
     }
 }
-window.onload=function(){
-    const adminLoginButton = document.getElementById('admin');
-    const userLoginButton = document.getElementById('user');
-    const container = document.getElementById('container');
-
-    adminLoginButton.addEventListener('click', () => {
-        container.classList.add("right-panel-active");
-    });
-    
-    userLoginButton.addEventListener('click', () => {
-        container.classList.remove("right-panel-active");
-    });
-}
-
 export default Login;
 
