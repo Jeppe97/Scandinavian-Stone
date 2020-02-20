c04e61b7414413f0294a9cad8d02397352a96c89
Yasin <yasin_97@live.se>
2020-02-20 11:22:21 +0100

    update

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './style.css';
+
+
+function Header() {
+    return(
+
+<header className="navbarHeader">Scandinavian Stone 
+
+
+
+</header>
+
+
+
+
+    )
+}
+
+
+export default Header;
