e1b15a1ab1f5351cc7859b5535c37560b8d868ab
Jesper <Jesperohman@outlook.com>
2020-02-26 10:57:55 +0100

    small

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "../styles/Login.scss";
-import { Link } from "react-router-dom";
 import auth from './auth';
 
 export class Login extends Component {    
